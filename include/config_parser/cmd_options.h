a8599feaa7578a20f1ed77346c64f888eba6cafa
Karol Czernuszka <kczernuszka@o2.pl>
2020-07-17 15:40:16 +0200

    Create cmd options

--- include/config_parser/cmd_options.h
+++ include/config_parser/cmd_options.h
@@ -1,19 +1,11 @@
-#ifndef XLSTOUSERS_CMDPARSER_CMDARGUMENTS_H
-#define XLSTOUSERS_CMDPARSER_CMDARGUMENTS_H
+#ifndef XLSTOUSERS_CONFIGPARSER_CMDOPTIONS_H
+#define XLSTOUSERS_CONFIGPARSER_CMDOPTIONS_H
 
-#include <string>
+#include <utility>
 
-namespace xlstousers { namespace config_parser 
-{ 
+const auto CONFIG_FILE = std::make_pair("config_file", "c");
+const auto INTERACTIVE = std::make_pair("interactive", "i");
+const auto TEST_MODE = std::make_pair("test", "t");
+const auto QUOTA = std::make_pair("quota", "q");
 
-struct CmdArguments {
-  std::string config_file;
-  std::string passwd_file;
-  bool interactive_mode;
-  bool test_mode;
-  bool quota;
-};
-
-}}  // namespace xlstousers::config_parser
-
-#endif  // XLSTOUSERS_CMDPARSER_CMDARGUMENTS_H
+#endif  // XLSTOUSERS_CONFIGPARSER_CMDOPTIONS_H
