19c6faef738fceb8c6156000806db5bd8b23c4db
Karol Czernuszka <kczernuszka@o2.pl>
2020-07-20 22:42:03 +0200

    Define type consts

--- include/config_parser/cmd_options_default_values.h
+++ include/config_parser/cmd_options_default_values.h
@@ -3,9 +3,9 @@
 
 #include "cmd_options.h"
 
-const auto DEFAULT_CONFIG_PATH = "/etc/xlstousers/";
-const auto DEFAULT_INTERACTIVE = false;
-const auto DEFAULT_TEST = false;
-const auto DEFAULT_QUOTA = false;
+const std::string DEFAULT_CONFIG_PATH = "/etc/xlstousers/";
+const bool DEFAULT_INTERACTIVE = false;
+const bool DEFAULT_TEST = false;
+const bool DEFAULT_QUOTA = false;
 
 #endif  // XLSTOUSERS_CONFIGPARSER_CMDOPTIONSDEFAULTVALUES_H
