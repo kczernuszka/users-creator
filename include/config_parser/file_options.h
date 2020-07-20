123925fd62cd27052363dd7bb9edd4e67e4a31ee
Karol Czernuszka <kczernuszka@o2.pl>
2020-07-20 22:27:32 +0200

    Change auto on string

--- include/config_parser/file_options.h
+++ include/config_parser/file_options.h
@@ -1,22 +1,24 @@
 #ifndef XLSTOUSERS_FILEPARSER_FILEOPTIONS_H
 #define XLSTOUSERS_FILEPARSER_FILEOPTIONS_H
 
-const auto COLUMNS = "columns_headers";
-const auto PASSWORD_LEN = "password_length";
-const auto MIN_UID = "initial_uid";
-const auto MAX_UID = "final_uid";
-const auto USER_GID = "gid";
-const auto HOME = "home_directory";
-const auto TIME = "time_to_password_change";
-const auto USER_CLASS = "class";
-const auto USER_SHELL = "shell";
-const auto EXPIRE = "account_expire";
-const auto BLOCKS_HARD_LIMIT = "block_hard_limit";
-const auto BLOCKS_SOFT_LIMIT = "block_soft_limit";
-const auto INODES_HARD_LIMIT = "inodes_hard_limit";
-const auto INODES_SOFT_LIMIT = "inodes_soft_limit";
-const auto BLOCKS_TIME = "block_time";
-const auto INODES_TIME = "inodes_time";
-const auto PATH_QUOTA = "path_quota";
+#include <string>
+
+const std::string COLUMNS = "columns_headers";
+const std::string PASSWORD_LEN = "password_length";
+const std::string MIN_UID = "initial_uid";
+const std::string MAX_UID = "final_uid";
+const std::string USER_GID = "gid";
+const std::string HOME = "home_directory";
+const std::string TIME = "time_to_password_change";
+const std::string USER_CLASS = "class";
+const std::string USER_SHELL = "shell";
+const std::string EXPIRE = "account_expire";
+const std::string BLOCKS_HARD_LIMIT = "block_hard_limit";
+const std::string BLOCKS_SOFT_LIMIT = "block_soft_limit";
+const std::string INODES_HARD_LIMIT = "inodes_hard_limit";
+const std::string INODES_SOFT_LIMIT = "inodes_soft_limit";
+const std::string BLOCKS_TIME = "block_time";
+const std::string INODES_TIME = "inodes_time";
+const std::string PATH_QUOTA = "path_quota";
 
 #endif  // XLSTOUSERS_FILEPARSER_FILEOPTIONS_H
