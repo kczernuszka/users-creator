fa283f50acf462ea0b83838b2aaf0676b646a55a
Karol Czernuszka <kczernuszka@o2.pl>
2020-07-27 23:33:14 +0200

    Remove using namespace directive

--- src/config_parser/unittests/option_value_parser_test.cpp
+++ src/config_parser/unittests/option_value_parser_test.cpp
@@ -2,13 +2,11 @@
 
 #include "../option_value_parser.h"
 
-using namespace xlstousers::config_parser;
-
 TEST(OptionValueParserTest, MultiValuesToVector) {
   std::string value = "name" + std::string(VALUES_DELIMITER) + "surname" +
                       std::string(VALUES_DELIMITER) + "workplace";
 
-  auto vectorStrings = valueToVector(value);
+  auto vectorStrings = xlstousers::config_parser::valueToVector(value);
 
   if (vectorStrings.size() != 3) FAIL();
   EXPECT_EQ(vectorStrings[0], "name");
