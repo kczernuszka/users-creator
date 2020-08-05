f8747a32edb97599e74c06a490a6a84fe1fdd96c
Karol Czernuszka <kczernuszka@o2.pl>
2020-08-05 17:22:30 +0200

    Create column reader factory

---
+++ src/users_reader/column_reader_factory.cpp
@@ -0,0 +1,36 @@
+// Copyright 2020, Karol Czernuszka
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+//
+
+#include "users_reader/column_reader_factory.h"
+
+namespace xlstousers {
+namespace users_reader {
+
+std::unique_ptr<IColumnReader> ColumnReader::getColumnReader(std::string file) {
+  std::string extension = getFileExtension(file);
+  if (extension == "xlsx")
+    return std::make_unique<XlsxColumnReader>(file);
+  else
+    return nullptr;
+}
+
+std::string ColumnReader::getFileExtension(std::string file) {
+  std::size_t found = file.find(".");
+  return file.substr(found+1);
+}
+
+}  // namespace users_reader
+}  // namespace xlstousers
