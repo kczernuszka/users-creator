16e346762a79c98bf41c1b4386814cdca620ab9d
Karol Czernuszka <kczernuszka@o2.pl>
2020-08-05 17:19:45 +0200

    Create column reader interface

---
+++ include/users_reader/i_column_reader.h
@@ -0,0 +1,34 @@
+// Copyright 2020, Karol Czernuszka
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+//
+
+#ifndef INCLUDE_USERS_READER_I_COLUMN_READER_H_
+#define INCLUDE_USERS_READER_I_COLUMN_READER_H_
+
+#include <vector>
+#include <string>
+
+namespace xlstousers {
+namespace users_reader {
+
+class IColumnReader {
+ public:
+  virtual std::vector<std::string> getColumn(std::string header) = 0;
+};
+
+}  // namespace users_reader
+}  // namespace xlstousers
+
+#endif  // INCLUDE_USERS_READER_I_COLUMN_READER_H_
